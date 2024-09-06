28cfafeacfa44fcb3d9b1be32c0dc04f06db903b
dlrlqor <137988673+NiceLeeMan@users.noreply.github.com>
2024-09-06 14:49:31 +0900

    Update RecoverUsernamePage.tsx

--- src/pages/authenticationPage/RecoverUsernamePage.tsx
+++ src/pages/authenticationPage/RecoverUsernamePage.tsx
@@ -7,48 +7,34 @@ import { FindUserIdFormData } from "../../types/User";
 import { handleInputChange } from "../../utils/state/useInputChange";
 
 
-
 const RecoverUsernamePage:React.FC = () =>{
 
     const [findUserIdData, setFindUserIdData] = useState<FindUserIdFormData>({
         email : ""
     })
 
-
     const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
         handleInputChange(e, findUserIdData, setFindUserIdData);
         console.log(findUserIdData)
     };
     
-
     const handleEmailSubmit = async () =>{
-
         const email = findUserIdData.email
         if(email ===""){
             alert("email을 입력하시오")
-
         }
-
         else{
-
             const apiUrl = "http://localhost:8080/api/auth/find-username"
-
+            
             try{
                 await sendDataToServer(findUserIdData, apiUrl)
                 alert("아이디를 전송하였습니다")
-            }
-
-            catch(error){
+            }catch(error){
                 console.error("아이디전송과정에서 오류 발생:", error);
             }
-
         }
-
     }
-
-
-
-
+    
     return(
         <BackgroundComponent>
             <InputComponent
@@ -59,8 +45,7 @@ const RecoverUsernamePage:React.FC = () =>{
                 onChange={handleEmailChange}
                 margin="0 0 0 0"
                 />
-
-
+            
             <ButtonComponent
                 text="전송" 
                 margin="60px 0 0 0"
@@ -68,11 +53,6 @@ const RecoverUsernamePage:React.FC = () =>{
         </BackgroundComponent>
         
     )
-
-
-
-
 }
-
 
 export default RecoverUsernamePage
